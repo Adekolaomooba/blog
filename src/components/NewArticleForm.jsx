ba77bad6084fd7628a2b133f928393d85e221c14
Adekolaomooba <adekola.adebayo2@gmail.com>
2025-02-12 10:56:12 +0100

    routing commit

--- src/components/NewArticleForm.jsx
+++ src/components/NewArticleForm.jsx
@@ -190,9 +190,9 @@ function NewArticleForm() {
 
 
     return (
-        <div className='w-full min-h-[85vh] bg-gray-500 '>
+        <div className='w-full min-h-[85vh] bg-gray-500 px-2 sm:px-[50px] '>
 
-            <div className=' mx-[100px] '>
+            <div>
                 <h3 className=' text-center text-3xl font-bold  '>
                     Add News Item
                 </h3>
